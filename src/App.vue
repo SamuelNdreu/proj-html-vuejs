665968d238ef28766a8961236145ed070c6d770f
SamuelNdreu <samwellsoficcial@gmail.com>
2023-01-18 16:07:03 +0100

    added footer as component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
   import AppHeader from './components/AppHeader.vue'
-  import AppMain from './components/AppMain.vue'
-  import AppFooter from './components/AppFooter.vue'
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
   export default {
     components: {
         AppHeader,
-        AppMain
+        AppMain,
+        AppFooter
     },
     data() {
         return {
